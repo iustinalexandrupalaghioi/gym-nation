96390f9e68f88f649ffc84b1b4884036fefb3bea
iustinalexandrupalaghioi <alex.palaghioi@gmail.com>
2024-03-28 11:29:26 +0200

    Deleting unnecessary images and creating modal structure

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -8,7 +8,7 @@ interface Props {
 const FeatureCard = ({ feature: { id, title, description, icon } }: Props) => {
   return (
     <div key={id} className="col d-flex flex-column gap-2">
-      <div className="icon p-2 bg-primary text-light fs-4 rounded-3">
+      <div className="icon p-2 bg-primary text-light text-center fs-4 rounded-3">
         {React.createElement(icon)}
       </div>
       <h4 className="fw-semibold mb-0 text-body-emphasis">{title}</h4>
--- src/components/ServicesCard.tsx
+++ src/components/ServicesCard.tsx
@@ -1,17 +1,24 @@
 import { Service } from "../data/services";
+import ServicesModal from "./ServicesModal";
 
 interface Props {
   service: Service;
 }
 const ServicesCard = ({ service: { id, title, img } }: Props) => {
   return (
-    <div className="col" key={id}>
+    <div
+      className="col"
+      key={id}
+      data-bs-toggle="modal"
+      data-bs-target="#exampleModal"
+    >
       <div className="card card-cover h-100 overflow-hidden rounded-4 shadow-lg">
         <img src={img} />
         <div className="layer d-flex flex-column h-100 p-5 pb-3 text-light text-shadow-1">
           <h3 className="display-6 lh-1 fw-bold text-center px-2">{title}</h3>
         </div>
       </div>
+      <ServicesModal />
     </div>
   );
 };
