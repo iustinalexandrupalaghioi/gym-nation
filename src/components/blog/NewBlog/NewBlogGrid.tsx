4e5fdd2b984711dad23dbf81b445b93fd5d1720d
iustinalexandrupalaghioi <alex.palaghioi@gmail.com>
2024-04-23 12:04:21 +0300

    new blog 100vh

--- src/components/blog/NewBlog/NewBlogGrid.tsx
+++ src/components/blog/NewBlog/NewBlogGrid.tsx
@@ -4,7 +4,7 @@ interface Props {
   children: ReactNode;
 }
 const NewBlogGrid = ({ children }: Props) => {
-  return <div className="container px-4 py-5">{children}</div>;
+  return <div className="container px-4 py-5 vh-100">{children}</div>;
 };
 
 export default NewBlogGrid;
