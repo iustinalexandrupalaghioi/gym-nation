24bef20f0e4747169c4cf1397e8e7cfe695e11e8
iustinalexandrupalaghioi <alex.palaghioi@gmail.com>
2024-05-15 12:44:44 +0300

    deleting unnecesary files

--- src/data/links.ts
+++
@@ -1,8 +0,0 @@
-const links = [
-  { id: 1, path: "/", name: "Acasă" },
-  { id: 2, path: "/blog", name: "Blog" },
-  { id: 3, path: "/services", name: "Servicii" },
-  { id: 4, path: "/login", name: "Autentificare" },
-];
-
-export default links;
--- src/hooks/useCategory.ts
+++
@@ -1,7 +0,0 @@
-import useCategories from "./useCategories";
-
-const useCategory = (slug: string) => {
-  const { data: categories } = useCategories();
-  return categories?.result.find((c) => c.data().slug === slug);
-};
-export default useCategory;
