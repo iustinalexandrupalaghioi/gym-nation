75b3033bd3ef0098c24f5960fdc74d58f34c52ad
iustinalexandrupalaghioi <alex.palaghioi@gmail.com>
2024-05-19 20:02:14 +0300

    modifying dots size for testimonial carousel

--- src/components/blog/BlogPosts/CategoryListItem.tsx
+++ src/components/blog/BlogPosts/CategoryListItem.tsx
@@ -14,17 +14,19 @@ const CategoryListItem = ({ doc }: Props) => {
   const { data: posts } = useFetchPostsNumber(doc.data().slug);
 
   return (
-    <li className="d-flex justify-content-between align-items-center">
+    <li className="d-flex justify-content-between align-items-center mb-2">
       <Link
         to="/blog"
         className={`nav-link ${
-          category === doc.data().slug ? "active" : "text-body-secondary"
+          category === doc.data().slug
+            ? "active fw-bold"
+            : "text-body-secondary"
         }`}
         onClick={() => setCategory(doc.data().slug)}
       >
         {doc.data().name}
       </Link>
-      <p className="text-primary fw-bold">{posts?.count}</p>
+      <p className="text-primary fw-bold mb-0">{posts?.count}</p>
     </li>
   );
 };
