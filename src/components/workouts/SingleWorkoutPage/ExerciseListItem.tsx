674b284c954c6c87700686f95cc4598db4ec4874
iustinalexandrupalaghioi <alex.palaghioi@gmail.com>
2024-06-10 20:44:52 +0300

    refactor

--- src/components/workouts/SingleWorkoutPage/ExerciseListItem.tsx
+++ src/components/workouts/SingleWorkoutPage/ExerciseListItem.tsx
@@ -1,25 +1,18 @@
+import Exercise from "../../../entities/Exercise";
+import "./singleWorkout.css";
 interface Props {
   exercise: Exercise;
   activeExercise: Exercise | undefined;
   setExercise: React.Dispatch<React.SetStateAction<Exercise | undefined>>;
 }
-import Exercise from "../../../entities/Exercise";
-import "./singleWorkout.css";
-const ExerciseListItem = ({
-  exercise,
-  activeExercise,
-
-  setExercise,
-}: Props) => {
+const ExerciseListItem = ({ exercise, activeExercise, setExercise }: Props) => {
   return (
     <li
       onClick={() => {
         setExercise(exercise);
       }}
       className={`list-group-item list-group-item-action cursor-pointer border-0 ${
-        activeExercise?.nameSlug === exercise.nameSlug
-          ? "active fw-bold"
-          : "text-body-secondary"
+        activeExercise?.nameSlug === exercise.nameSlug ? "active" : null
       }`}
     >
       {exercise.name}
