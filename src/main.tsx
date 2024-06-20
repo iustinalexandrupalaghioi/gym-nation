75ae6db0d7f3e6f1a125072b23b9d6881925a3f2
iustinalexandrupalaghioi <alex.palaghioi@gmail.com>
2024-06-20 13:59:19 +0300

    verify user

--- src/main.tsx
+++ src/main.tsx
@@ -8,22 +8,7 @@ import router from "./routes.tsx";
 import "./assets/styles/_custom.scss";
 import "bootstrap";
 import "./index.css";
-import getUserStatus from "./utilities/getUserStatus.ts";
-import { auth } from "./firebase-config.ts";
-import getUserRole from "./utilities/getUserRole.ts";
-import useUserStatusStore from "./stores/userStore.ts";
-
 export const queryClient = new QueryClient();
-const setStatus = useUserStatusStore((s) => s.setStatus);
-const setRole = useUserStatusStore((s) => s.setRole);
-const newUserStatus = auth.currentUser ? await getUserStatus() : false;
-setStatus(newUserStatus);
-
-//check user role
-const newUserRole = auth.currentUser
-  ? getUserRole(auth.currentUser.uid)
-  : false;
-setRole(newUserRole);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- src/pages/Client/HomePage.tsx
+++ src/pages/Client/HomePage.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import ToastAlert from "../../components/ToastAlert";
 import AboutSection from "../../components/homepage/About";
 import ContactGrid from "../../components/homepage/Contact/ContactGrid";
@@ -13,8 +14,27 @@ import TeamGrid from "../../components/homepage/Team/TeamGrid";
 import TestimonialsGrid from "../../components/homepage/Testimonial/TestimonialsGrid";
 import Footer from "../../components/partials/Footer/Footer";
 import NavBar from "../../components/partials/Navbar/NavBar";
+import { auth } from "../../firebase-config";
+import useUserStatusStore from "../../stores/userStore";
+import getUserRole from "../../utilities/getUserRole";
+import getUserStatus from "../../utilities/getUserStatus";
 
 const HomePage = () => {
+  useEffect(() => {
+    const verifyUser = async () => {
+      const newUserStatus = auth.currentUser ? await getUserStatus() : false;
+      const newUserRole = auth.currentUser
+        ? getUserRole(auth.currentUser.uid)
+        : false;
+      const setStatus = useUserStatusStore((s) => s.setStatus);
+      const setRole = useUserStatusStore((s) => s.setRole);
+
+      setStatus(newUserStatus);
+      setRole(newUserRole);
+    };
+    verifyUser();
+  }, [auth.currentUser?.uid]);
+
   return (
     <>
       <ToastAlert />
